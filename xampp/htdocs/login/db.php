8f1d14363185c3a4871f242d01a7f00a8ad86ce0
AlexRoessner1986 <alexander.roessner@web.de>
2017-05-27 16:29:20 +0200

    Update db.php

--- xampp/htdocs/login/db.php
+++ xampp/htdocs/login/db.php
@@ -2,7 +2,7 @@
 /* Datenbankverbindung*/
 $host = 'localhost';
 $user = 'root';
-$pass = 'phpmyadminpasswort';
-$db = 'beuthportal';
+$pass = 'phpmyadminpasswort'; //das müsst ihr auf euer xampp/phpmyadmin-Passwort ändern
+$db = 'beuthportal'; //die müsst ihr lokal anlegen bei phpmyadmin, siehe die beiden files im Unterordner "sql"
 $mysqli = new mysqli($host,$user,$pass,$db) or die($mysqli->error);
 
