ac265ac931b0ad926fb68e4bafa923dcb1294dc2
AlexRoessner1986 <alexander.roessner@web.de>
2017-09-17 19:21:48 +0200

    Test CI

--- xampp/htdocs/tests/LoginTest.php
+++ xampp/htdocs/tests/LoginTest.php
@@ -2,19 +2,38 @@
 
 class LoginTest extends PHPUnit_Framework_TestCase
 {
-     //TESTET DIE DATENBANKVERBINDUNG UND OB EINTRÄGE VORHANDEN SIND
+     //TESTET DIE LOGIN-FUNKTIONEN
 	 public function testLogin()
 	{
+		//Test der Stringlänge-Funktion
 		$binary_string = 'Test';
 		function _strlen($binary_string) {
             if (function_exists('mb_strlen')) {
                 return mb_strlen($binary_string, '8bit');
             }
             return strlen($binary_string);
-        }
-		
+        }		
 		$this->assertNotNull($binary_string);
-		 $this->assertEquals(4, strlen($binary_string));
+		$this->assertEquals(4, strlen($binary_string));
+		
+		
+		
+		//TEst der Substring-Funktion
+		$start = 1;
+		$length = 2;
+		function _substr($binary_string, $start, $length) {
+            if (function_exists('mb_substr')) {
+                return mb_substr($binary_string, $start, $length, '8bit');
+            }
+            return substr($binary_string, $start, $length);
+        }
+		$this->assertNotNull($binary_string);
+		$this->assertEquals('es', substr($binary_string, $start, $length));
+		
+		
+		
+		
+		
 	}
  
 
