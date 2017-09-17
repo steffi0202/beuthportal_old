34f075feb15fba925d31d72e84c00535b54036a7
AlexRoessner1986 <alexander.roessner@web.de>
2017-09-17 19:41:54 +0200

    Unit-Test Passwort

--- xampp/htdocs/tests/RegisterTest.php
+++ xampp/htdocs/tests/RegisterTest.php
@@ -9,7 +9,7 @@ class RegisterTest extends PHPUnit_Framework_TestCase
 		$email = 's433948@beuth-hochschule.de';
 		list ($user, $domain) = explode('@', $email);
 		if($domain !== 'beuth-hochschule.de'){
-			echo "<font color='#FF0000'>Deine E-Mail-Adresse ist keine gültige Beuth-Email-Adresse!<br></font>";
+			//echo "<font color='#FF0000'>Deine E-Mail-Adresse ist keine gültige Beuth-Email-Adresse!<br></font>";
 			$error = true;
 		}			
 		$this->assertNotTrue($error);
@@ -17,6 +17,18 @@ class RegisterTest extends PHPUnit_Framework_TestCase
 	}
 	
 	
+	 //Test der Passwortprüfung
+	 public function testPassword()
+	{	
+		$error = false;
+		$passwort = '8757KKKKhhhhdjkfk';
+		if (!preg_match('/[A-Z]/', $passwort) OR !preg_match('/[a-z]/', $passwort) OR !preg_match('/[0-9]/', $passwort)  OR strlen($passwort) < 8) {
+			//echo "<font color='#FF0000'>Das Passwort muss mindestens 8 Zeichen lang sein und aus Zahlen, Klein- und Grossbuchstaben bestehen<br></font>";
+			$error = true;
+		}
+		$this->assertNotTrue($error);
+	
+	}
 		
 }
  
